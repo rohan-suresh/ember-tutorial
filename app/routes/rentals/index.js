722bae1b4ba064504e2ac3b2eb6bd4227be75618
zybooks <zybooks@zybookss-MacBook-Air.local>
2017-05-25 11:31:05 -0700

    Ember tutorial

---
+++ app/routes/rentals/index.js
@@ -0,0 +1,9 @@
+import RentalsController from '../rentals';
+
+export default RentalsController;
+
+//export default Ember.Route.extend({
+  //model() {
+    //return this.get('store').findAll('rental');
+  //}
+//});
---
+++ app/routes/rentals/show.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+export default Ember.Route.extend({
+  model(params) {
+    return this.get('store').findRecord('rental', params.rental_id);
+  }
+});
